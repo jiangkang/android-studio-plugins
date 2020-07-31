7c70e44ee726f7daf9ab0b072eeff842338f95f7
GitHub Action <action@github.com>
2020-07-31 14:45:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jiangkang/androidstudioplugins/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jiangkang.androidstudioplugins.services
+
+import com.github.jiangkang.androidstudioplugins.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
